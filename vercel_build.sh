b8e2b11e0ce555104b56a0d32269a7dd3fe5714f
DoanHieu1308 <hieudoan.13082003@gmail.com>
2025-09-20 16:48:58 +0700

    Fix line endings v1

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-set -e
+# Download Flutter SDK
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
 
-# Ensure LF line endings only
-# Run this on your machine before pushing:
-#   dos2unix vercel_build.sh
-
-echo "Downloading Flutter SDK..."
-git clone --depth 1 https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:$(pwd)/flutter/bin"
-
-echo "Flutter version:"
 flutter --version
-
-# Go to project directory (where pubspec.yaml is)
-cd $(dirname "$0")
-
-echo "Getting dependencies..."
 flutter pub get
-
-echo "Building for web..."
-flutter build web --release --base-href /
-
-echo "Build completed successfully!"
+flutter build web --release
